1a462eadcdc481fa1dcdc48af193d0831856d305
Ander Liu <38692285+AQZ0216@users.noreply.github.com>
2018-05-11 06:57:15 +0800

    save

--- Det.v
+++ Det.v
@@ -1,3 +1,38 @@
-module MM(clk,i,j,reset,read,write,index,read_data,write_data,finish);
+`define S0 2'b00
+`define S1 2'b01
 
+module MM(clk,i,j,reset,read,write,read_data,write_data,finish);
+	
+	input clk;
+	input reset;
+	input [19:0] read_data;
+	
+	output [19:0] i, j;
+	output read, write;
+	output [39:0] write_data;
+	output finish;
+	
+	reg [1:0] state, next_state;
+	
+	always @(posedge clk or posedge reset)
+	begin
+		if(reset == 1'b1)
+		begin
+			state <= `S0;	
+		end
+		else
+		begin
+			state <= next_state;
+		end
+	end
+	
+	always @*
+	begin
+		case(state)
+			`S0: begin
+				
+			end
+		endcase
+	end
+	
 endmodule
