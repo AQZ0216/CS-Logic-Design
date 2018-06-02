766c75394428f9dea863ad1c7cc9db7a5d54a8c6
Ander Liu <38692285+AQZ0216@users.noreply.github.com>
2018-06-03 03:26:26 +0800

    25bits

--- IIR.v
+++ IIR.v
@@ -1,14 +1,14 @@
-`define a5	32'b00000000000000000000010011111001
-`define a4	32'b00000000000000000000010101100111
-`define a3	32'b00000000000000000000100110100111
-`define a2	32'b00000000000000000000100110100111
-`define a1	32'b00000000000000000000010101100111
-`define a0	32'b00000000000000000000010011111001
-`define b4	32'b11111111111111010011110111110100
-`define b3	32'b00000000000001000000001011010000
-`define b2	32'b11111111111111001010000100000000
-`define b1	32'b00000000000000011010011101111001
-`define b0	32'b11111111111111111001111011010100
+`define a5	25'b00000000000010011111001
+`define a4	25'b00000000000010101100111
+`define a3	25'b00000000000100110100111
+`define a2	25'b00000000000100110100111
+`define a1	25'b00000000000010101100111
+`define a0	25'b00000000000010011111001
+`define b4	25'b00000101100001000001100
+`define b3	25'b00001000000001011010000
+`define b2	25'b00000110101111100000000
+`define b1	25'b00000011010011101111001
+`define b0	25'b00000000110000100101100
 
 module IIR(clk,rst,load,DIn,RAddr,data_done,WEN,Yn,WAddr,Finish);
 
@@ -24,11 +24,11 @@ module IIR(clk,rst,load,DIn,RAddr,data_done,WEN,Yn,WAddr,Finish);
 	wire [19:0] next_RAddr, next_WAddr;
 	wire next_Finish;
 	
-	reg [15:0] s0, s1, s2, s3, s4;
-	reg [15:0] new_s0, new_s1, new_s2, new_s3, new_s4;
-	wire [15:0] next_s0, next_s1, next_s2, next_s3, next_s4;
-	wire [15:0] next_new_s0, next_new_s1, next_new_s2, next_new_s3, next_new_s4;
-	wire [31:0] ans;
+	reg [24:0] s0, s1, s2, s3, s4;
+	reg [24:0] new_s0, new_s1, new_s2, new_s3, new_s4;
+	wire [24:0] next_s0, next_s1, next_s2, next_s3, next_s4;
+	wire [24:0] next_new_s0, next_new_s1, next_new_s2, next_new_s3, next_new_s4;
+	wire [24:0] sum;
 	
 	always @(posedge clk or posedge rst)
 	begin
@@ -37,16 +37,16 @@ module IIR(clk,rst,load,DIn,RAddr,data_done,WEN,Yn,WAddr,Finish);
 			RAddr <= 20'd0;
 			WAddr <= 20'd0;
 			Finish <= 1'b0;
-			s0 <= 16'd0;
-			s1 <= 16'd0;
-			s2 <= 16'd0;
-			s3 <= 16'd0;
-			s4 <= 16'd0;
-			new_s0 <= 16'd0;
-			new_s1 <= 16'd0;
-			new_s2 <= 16'd0;
-			new_s3 <= 16'd0;
-			new_s4 <= 16'd0;
+			s0 <= 25'd0;
+			s1 <= 25'd0;
+			s2 <= 25'd0;
+			s3 <= 25'd0;
+			s4 <= 25'd0;
+			new_s0 <= 25'd0;
+			new_s1 <= 25'd0;
+			new_s2 <= 25'd0;
+			new_s3 <= 25'd0;
+			new_s4 <= 25'd0;
 		end
 		else
 		begin
@@ -77,16 +77,17 @@ module IIR(clk,rst,load,DIn,RAddr,data_done,WEN,Yn,WAddr,Finish);
 	assign next_s1 = s2;
 	assign next_s2 = s3;
 	assign next_s3 = s4;
-	assign next_s4 = DIn;
+	assign next_s4 = {{2{DIn[15]}}, DIn, {7{1'b0}}};
 	
 	assign next_new_s0 = new_s1;
 	assign next_new_s1 = new_s2;
 	assign next_new_s2 = new_s3;
 	assign next_new_s3 = new_s4;
-	assign next_new_s4 = Yn;
+	assign next_new_s4 = sum; 
 	
-	assign ans = `a0*{{16{s0[15]}}, s0} + `a1*{{16{s1[15]}}, s1} + `a2*{{16{s2[15]}}, s2} + `a3*{{16{s3[15]}}, s3} + `a4*{{16{s4[15]}}, s4} + `a5*{{16{DIn[15]}}, DIn} 
-		- `b0*{{16{new_s0[15]}}, new_s0} - `b1*{{16{new_s1[15]}}, new_s1} - `b2*{{16{new_s2[15]}}, new_s2} - `b3*{{16{new_s3[15]}}, new_s3} - `b4*{{16{new_s4[15]}}, new_s4}; 
-	assign Yn = ans[31:16];
+	assign sum = `a0*s0 + `a1*s1 + `a2*s2 + `a3*s3 + `a4*s4 + `a5*{{2{DIn[15]}}, DIn, {7{1'b0}}}
+		+ `b0*new_s0 - `b1*new_s1 + `b2*new_s2- `b3*new_s3 + `b4*new_s4;
+	
+	assign Yn =  {sum[24], sum[21:7]};
 	
 endmodule
